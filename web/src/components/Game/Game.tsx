33eef1db6b9b9697d8be785c90cb6966b089f8e7
Keane Kwa <hello@keanekwa.com>
2023-01-12 11:24:12 -0800

    Fix React imports

--- web/src/components/Game/Game.tsx
+++ web/src/components/Game/Game.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import random from "random"
 import Timer from "./Timer/Timer"
 import Score from "./Score/Score"
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import Layout from "./components/Layout/Layout"
