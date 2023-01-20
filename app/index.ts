28fc1f2af1cc70b2913d600e822e49cb39cfdbde
Keane Kwa <hello@keanekwa.com>
2023-01-19 18:55:05 -0800

    Update .env for PROD

--- app/index.ts
+++ app/index.ts
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(
 	cors({
-		origin: process.env.WEB_HOST + ":" + process.env.WEB_PORT,
+		origin: process.env.ENV === "PROD" ? process.env.WEB_HOST : process.env.WEB_HOST + ":" + process.env.WEB_PORT,
 		credentials: true
 	})
 )
