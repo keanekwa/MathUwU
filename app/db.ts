15b830368ae3f2b4440424f33d99db6da5e40afd
Keane Kwa <hello@keanekwa.com>
2023-01-19 17:25:51 -0800

    update .env for deployment

--- app/db.ts
+++ app/db.ts
@@ -4,11 +4,11 @@ const Pool = require("pg").Pool
 dotenv.config()
 
 const pool = new Pool({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_DATABASE,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD
+	host: process.env.PGHOST,
+	database: process.env.PGDATABASE,
+	port: process.env.PGPORT,
+	user: process.env.PGUSER,
+	password: process.env.PGPASSWORD
 })
 
 module.exports = pool
--- app/index.ts
+++ app/index.ts
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(
 	cors({
-		origin: "http://localhost:3001",
+		origin: process.env.WEB_HOST + ":" + process.env.WEB_PORT,
 		credentials: true
 	})
 )
