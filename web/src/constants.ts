73f9780d4ba79b3099de4fccd5ed8c31a4486d7b
Keane Kwa <hello@keanekwa.com>
2023-01-19 16:31:13 -0800

    Update game mode descriptions

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -4,28 +4,25 @@ export const GAME_MODES: { [key: string]: IGameMode } = {
 	DEFAULT: {
 		path: "/default",
 		name: "Default",
-		description: "The default arithmetic game with whole numbers.",
+		description: "The default Zetamac-style arithmetic game with whole numbers.",
 		disabled: false
 	},
 	DECIMALS: {
 		path: "/decimals",
 		name: "Decimals",
-		// description: "Arithmetic game with decimals.",
-		description: "Coming soon.",
+		description: "[Coming soon] Arithmetic game with decimals.",
 		disabled: true
 	},
 	FRACTIONS: {
 		path: "/fractions",
 		name: "Fractions",
-		// description: "Arithmetic game with fractions.",
-		description: "Coming soon.",
+		description: "[Coming soon] Arithmetic game with fractions.",
 		disabled: true
 	},
 	EIGHTY_IN_EIGHT: {
 		path: "/80in8",
 		name: "80 in 8",
-		// description: "Optiver's 80 in 8.",
-		description: "Coming soon.",
+		description: "[Coming soon] Optiver's 80 in 8 assessment. Solve 80 questions in 8 minutes.",
 		disabled: true
 	}
 }
