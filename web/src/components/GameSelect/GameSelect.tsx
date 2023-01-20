09434aa5ec2bc9c793c951d52679de8efef307db
Keane Kwa <hello@keanekwa.com>
2023-01-19 17:40:24 -0800

    Testing CICD auto build

--- web/src/components/GameSelect/GameSelect.tsx
+++ web/src/components/GameSelect/GameSelect.tsx
@@ -5,7 +5,7 @@ import GameSelectButton from "./GameSelectButton/GameSelectButton"
 const GameSelect = () => {
 	return (
 		<div>
-			<h2 className="text-xl text-center font-light mt-5 mb-14">Select a game mode</h2>
+			<h2 className="text-xl text-center font-light mt-5 mb-14">Select a game mode:</h2>
 			<div className="grid grid-cols-2 gap-12">
 				{Object.values(GAME_MODES).map((mode) => (
 					<GameSelectButton key={mode.name} mode={mode} />
