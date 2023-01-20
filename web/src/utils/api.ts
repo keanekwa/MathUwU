300a13c9ff5443805a7d7d92f318f6f2b8a49960
Keane Kwa <hello@keanekwa.com>
2023-01-19 19:32:39 -0800

    Fix PROD api CORS

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios"
 
 const api = axios.create({
-	baseURL: process.env.REACT_APP_API_HOST! + ":" + process.env.REACT_APP_API_PORT!.toString(),
+	baseURL:
+		process.env.ENV === "PROD"
+			? process.env.REACT_APP_API_HOST
+			: process.env.REACT_APP_API_HOST! + ":" + process.env.REACT_APP_API_PORT!.toString(),
 	timeout: 1000,
 	withCredentials: true
 })
