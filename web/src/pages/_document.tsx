76caf9299e26e89786fdb770fe2a745dd0830782
Keane Kwa <hello@keanekwa.com>
2023-02-19 15:27:08 -0800

    Fix %PUBLIC_URL%

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -6,12 +6,11 @@ const Document = () => {
 		<Html lang="en">
 			<Head>
 				<meta charSet="utf-8" />
-				<link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-				<link rel="icon" type="image/png" sizes="32x32" href="%PUBLIC_URL%/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="%PUBLIC_URL%/favicon-16x16.png" />
-				<link rel="apple-touch-icon" sizes="180x180" href="%PUBLIC_URL%/apple-touch-icon.png" />
-				<link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-				<meta name="theme-color" content="#ffffff" />
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="manifest" href="/manifest.json" />
 				<meta
 					name="description"
 					content="Gamified training platform for quantitative finance interviews. Challenge yourself to a series of mental arithmetic games to prepare for the quant world."
