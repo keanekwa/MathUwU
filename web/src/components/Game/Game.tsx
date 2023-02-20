10ed696cd91367295c185f5ffe10eadb3eaecf23
Keane Kwa <hello@keanekwa.com>
2023-02-19 23:39:18 -0800

    create game.function.ts

--- web/src/components/Game/Game.tsx
+++ web/src/components/Game/Game.tsx
@@ -1,62 +1,17 @@
 import React, { useContext, useEffect, useRef, useState } from "react"
 
 import api from "../../lib/utils/api"
-import random from "random"
 import useInterval from "../../lib/utils/useInterval"
 import Timer from "./Timer/Timer"
 import Score from "./Score/Score"
 import CustomSettings from "./CustomSettings/CustomSettings"
-import { GAME_MODES, OPERATORS } from "../../lib/constants/game.constants"
+import { GAME_MODES } from "../../lib/constants/game.constants"
 import Stats from "./Stats/Stats"
-import { IQuestionAnswered, ISettings } from "../../lib/interfaces/game.interfaces"
+import { IQuestionAnswered } from "../../lib/interfaces/game.interfaces"
 import { useRouter } from "next/router"
 import { UserContext } from "../../lib/contexts/user.context"
 import { AlertContext } from "../../lib/contexts/alert.context"
-
-const getNumbers = (operator: string, settings: ISettings) => {
-	const { add1, add2, multiply1, multiply2 } = settings
-
-	const returnNormal = (a: number, b: number, ans: number) => {
-		return { vars: [a, b], ans: ans }
-	}
-
-	const returnReverse = (a: number, b: number, ans: number) => {
-		return { vars: [ans, a], ans: b }
-	}
-
-	if (operator === OPERATORS.ADD || operator === OPERATORS.SUBTRACT) {
-		const a = random.int(Math.min(...add1), Math.max(...add1))
-		const b = random.int(Math.min(...add2), Math.max(...add2))
-		const ans = a + b
-
-		if (operator === OPERATORS.ADD) return returnNormal(a, b, ans)
-		if (operator === OPERATORS.SUBTRACT) return returnReverse(a, b, ans)
-	}
-
-	if (operator === OPERATORS.MULTIPLY || operator === OPERATORS.DIVIDE) {
-		const a = random.int(Math.min(...multiply1), Math.max(...multiply1))
-		const b = random.int(Math.min(...multiply2), Math.max(...multiply2))
-		const ans = a * b
-
-		if (operator === OPERATORS.MULTIPLY) return returnNormal(a, b, ans)
-		if (operator === OPERATORS.DIVIDE) return returnReverse(a, b, ans)
-	}
-}
-
-const getQuestion = (settings: ISettings) => {
-	const { isAdd, isSubtract, isMultiply, isDivide } = settings
-	let operators: Array<string> = []
-
-	if (isAdd) operators.push(OPERATORS.ADD)
-	if (isSubtract) operators.push(OPERATORS.SUBTRACT)
-	if (isMultiply) operators.push(OPERATORS.MULTIPLY)
-	if (isDivide) operators.push(OPERATORS.DIVIDE)
-
-	const operator = random.choice(operators) as string
-	const numbers = getNumbers(operator, settings)
-
-	return { operator: operator, numbers: numbers }
-}
+import { getQuestion } from "../../lib/functions/game.function"
 
 const Game = () => {
 	const router = useRouter()
---
+++ web/src/lib/functions/game.function.ts
@@ -0,0 +1,48 @@
+import random from "random"
+import { OPERATORS } from "@/lib/constants/game.constants"
+import { ISettings } from "@/lib/interfaces/game.interfaces"
+
+const getNumbers = (operator: string, settings: ISettings) => {
+	const { add1, add2, multiply1, multiply2 } = settings
+
+	const returnNormal = (a: number, b: number, ans: number) => {
+		return { vars: [a, b], ans: ans }
+	}
+
+	const returnReverse = (a: number, b: number, ans: number) => {
+		return { vars: [ans, a], ans: b }
+	}
+
+	if (operator === OPERATORS.ADD || operator === OPERATORS.SUBTRACT) {
+		const a = random.int(Math.min(...add1), Math.max(...add1))
+		const b = random.int(Math.min(...add2), Math.max(...add2))
+		const ans = a + b
+
+		if (operator === OPERATORS.ADD) return returnNormal(a, b, ans)
+		if (operator === OPERATORS.SUBTRACT) return returnReverse(a, b, ans)
+	}
+
+	if (operator === OPERATORS.MULTIPLY || operator === OPERATORS.DIVIDE) {
+		const a = random.int(Math.min(...multiply1), Math.max(...multiply1))
+		const b = random.int(Math.min(...multiply2), Math.max(...multiply2))
+		const ans = a * b
+
+		if (operator === OPERATORS.MULTIPLY) return returnNormal(a, b, ans)
+		if (operator === OPERATORS.DIVIDE) return returnReverse(a, b, ans)
+	}
+}
+
+export const getQuestion = (settings: ISettings, mode: string) => {
+	const { isAdd, isSubtract, isMultiply, isDivide } = settings
+	let operators: Array<string> = []
+
+	if (isAdd) operators.push(OPERATORS.ADD)
+	if (isSubtract) operators.push(OPERATORS.SUBTRACT)
+	if (isMultiply) operators.push(OPERATORS.MULTIPLY)
+	if (isDivide) operators.push(OPERATORS.DIVIDE)
+
+	const operator = random.choice(operators) as string
+	const numbers = getNumbers(operator, settings)
+
+	return { operator: operator, numbers: numbers }
+}
