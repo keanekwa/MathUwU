c4f58073e7346655e6872ee7038091d7cb07c759
Keane Kwa <hello@keanekwa.com>
2023-02-22 13:59:09 -0800

    Disallow negative game duration

--- web/src/components/Game/Game.tsx
+++ web/src/components/Game/Game.tsx
@@ -101,6 +101,10 @@ const Game = () => {
 			setAlert({ show: true, message: "Enter a numeric range for multiplication." })
 			return
 		}
+		if (seconds < 0) {
+			setAlert({ show: true, message: "Game duration cannot be negative." })
+			return
+		}
 		if (Number.isNaN(seconds)) {
 			setAlert({ show: true, message: "Enter a game duration." })
 			return
