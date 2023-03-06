38fe460f64dfe33b4c88f1793ff22920c097a859
Keane Kwa <hello@keanekwa.com>
2023-03-06 00:57:49 -0800

    update meta

--- web/src/pages/game/[mode].tsx
+++ web/src/pages/game/[mode].tsx
@@ -1,11 +1,21 @@
 import Game from "@/components/Game/Game"
 import Layout from "@/components/Layout/Layout"
+import Head from "next/head"
 
 const Mode = () => {
 	return (
-		<Layout>
-			<Game />
-		</Layout>
+		<>
+			<Head>
+				<title>MathUwU | Arithmetic Zetamac & Quant Interviews Made Fun</title>
+				<meta
+					name="description"
+					content="MathUwU is a gamified quantitative finance interview preparation platform, with mental math games like Arithmetic Zetamac and other brainteasers."
+				/>
+			</Head>
+			<Layout>
+				<Game />
+			</Layout>
+		</>
 	)
 }
 
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,21 @@
 import Layout from "@/components/Layout/Layout"
 import GameSelect from "@/components/GameSelect/GameSelect"
+import Head from "next/head"
 
 const Index = () => {
 	return (
-		<Layout>
-			<GameSelect />
-		</Layout>
+		<>
+			<Head>
+				<title>MathUwU | Arithmetic Zetamac & Quant Interviews Made Fun</title>
+				<meta
+					name="description"
+					content="MathUwU is a gamified quantitative finance interview preparation platform, with mental math games like Arithmetic Zetamac and other brainteasers."
+				/>
+			</Head>
+			<Layout>
+				<GameSelect />
+			</Layout>
+		</>
 	)
 }
 
